5d823751fd7e111f7fe60ffc7f3f54de5734e17f
tushal-pethani <127192816+tushal-pethani@users.noreply.github.com>
2024-11-19 03:50:05 +0530

    Added path for cv generation

--- FusionIIIT/applications/placement_cell/api/urls.py
+++ FusionIIIT/applications/placement_cell/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PlacementScheduleView,BatchStatisticsView
+from .views import *
 
 
 urlpatterns = [
     path('placement/', PlacementScheduleView.as_view(), name='placement-list'),
     path('statistics/',BatchStatisticsView.as_view()),
+    path('generate-cv/',generate_cv ,name='generate_cv'),
 ]
