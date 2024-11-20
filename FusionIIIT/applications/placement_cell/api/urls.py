d8dd41d23436b0dec927ad35095beb3ecd20b360
Neyati <116624667+Doraemon012@users.noreply.github.com>
2024-11-20 17:42:22 +0530

    Update urls.py

--- FusionIIIT/applications/placement_cell/api/urls.py
+++ FusionIIIT/applications/placement_cell/api/urls.py
@@ -5,5 +5,5 @@ from .views import *
 urlpatterns = [
     path('placement/', PlacementScheduleView.as_view(), name='placement-list'),
     path('statistics/',BatchStatisticsView.as_view()),
-    path('generate-cv/',generate_cv ,name='generate_cv'),
+    path('generate-cv/',generate_cv.as_view(),name='generate_cv'),
 ]
